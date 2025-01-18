bc19262de0ddb2a2525fefae00394c35de01a3e4
Taranpreet Singh <70011707+Taranpreet1407@users.noreply.github.com>
2025-01-18 18:32:06 +0530

    Remove Duplicates From String Using Recursion
    Remove Duplicates From String Using Recursion

--- Application.java
+++ Application.java
@@ -2,28 +2,54 @@ package main;
 
 
 public class Application {
+	public static boolean map[]=new boolean[26];
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String myString="abpor";
-		String revString="";
-		Stringrev(myString,revString,myString.length()-1);
+		String myString="aabccbadd";
+		String newString="";
+		//Stringrev(myString,newString,myString.length()-1);
+		
+		RemoveDuplicates(myString,newString,0);
 		
 
 	}
 	
 	// Reversing the String Using Recurssion
-	public static  void Stringrev(String myString,String revString,int n) {
+	public static  void Stringrev(String myString,String newString,int n) {
 		if (n==0) {
 			
-			revString += myString.charAt(n);
-			System.out.println("Rev. String is- > "+revString);
+			newString += myString.charAt(n);
+			System.out.println("Rev. String is- > "+newString);
 			return ;
 		}
 		
-		revString += myString.charAt(n);
-		 Stringrev(myString,revString,n-1);
+		newString += myString.charAt(n);
+		 Stringrev(myString,newString,n-1);
 
 	}
-
+	
+	public static void RemoveDuplicates (String myString, String newString,int indx){
+		
+		if (indx==myString.length()) {
+			System.out.println("New String Is -> "+ newString);
+			return;
+		}
+		
+		char currChar = myString.charAt(indx);
+		if (map[currChar-'a']) {
+			RemoveDuplicates(myString,newString,indx+1);
+			
+		} else {
+			
+			newString += currChar;
+			map[currChar-'a']=true;
+			RemoveDuplicates(myString,newString,indx+1);
+			
+			
+		}
+		
+		
+		
+	}
 }
